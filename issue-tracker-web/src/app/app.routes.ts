378f8779b1379731e42a282bea2c0e6e4ac3644b
messified <jesse.c.reese@gmail.com>
2026-01-03 16:28:09 -0700

    added application layout with sidebar navigation and routed content shell

--- issue-tracker-web/src/app/app.routes.ts
+++ issue-tracker-web/src/app/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
 import { ProjectComponent } from './components/project/project.component';
 import { IssueComponent } from './components/issue/issue.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
-  { path: 'projects', component: ProjectComponent },
-  { path: 'issues', component: IssueComponent },
   {
-    path: 'issues/:id',
-    loadComponent: () =>
-      import('./components/issue-detail/issue-detail.component')
-        .then(m => m.IssueDetailComponent)
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'projects', component: ProjectComponent },
+      { path: 'issues', component: IssueComponent },
+      {
+        path: 'issues/:id',
+        loadComponent: () =>
+          import('./components/issue-detail/issue-detail.component')
+            .then(m => m.IssueDetailComponent)
+      },
+      { path: '', redirectTo: 'projects', pathMatch: 'full' }
+    ]
   },
-  { path: '', redirectTo: 'projects', pathMatch: 'full' },
-  { path: '**', redirectTo: 'projects' }
+  { path: '**', redirectTo: '' }
 ];
---
+++ issue-tracker-web/src/app/components/layout/layout.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent {}
