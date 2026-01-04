16ed7d7aae4c159503545c59e6f7b594c7ead9f3
messified <jesse.c.reese@gmail.com>
2026-01-03 17:13:42 -0700

    load issue detail data from IssueService by route id

--- issue-tracker-web/src/app/components/issue-detail/issue-detail.component.ts
+++ issue-tracker-web/src/app/components/issue-detail/issue-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+
+import { IssueService } from '../../services/issue.service';
+import { Issue } from '../../models/issue.model';
 
 @Component({
   selector: 'app-issue-detail',
@@ -15,19 +18,33 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './issue-detail.component.html',
   styleUrl: './issue-detail.component.scss'
 })
-export class IssueDetailComponent {
+export class IssueDetailComponent implements OnInit {
 
   issueId!: number;
+  issue?: Issue;
+  loading = true;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
-  ) {
+    private router: Router,
+    private issueService: IssueService
+  ) {}
+
+  ngOnInit(): void {
     this.issueId = Number(this.route.snapshot.paramMap.get('id'));
+    this.loadIssue();
+  }
+
+  loadIssue(): void {
+    this.issueService.getIssue(this.issueId).subscribe({
+      next: issue => {
+        this.issue = issue;
+        this.loading = false;
+      }
+    });
   }
 
   goBackToList(): void {
     this.router.navigate(['/issues']);
   }
-
 }
